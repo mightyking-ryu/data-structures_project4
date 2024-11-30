9992fc71273f28445114f35d227890bd572ad41a
mightyking-ryu <dongwooryu@snu.ac.kr>
2024-11-30 16:35:34 +0900

    Implement put method in HashTable class

--- include/hash_table.hpp
+++ include/hash_table.hpp
@@ -81,7 +81,26 @@ int HashTable<K, V>::get(const K &key, V &value) {
 template <typename K, typename V>
 int HashTable<K, V>::put(const K &key, const V &value) {
     // TODO
-    return -1;
+    unsigned int pos = this->get_pos(key);
+    unsigned int next_pos = pos;
+    int i = 0;
+    while(this->table[next_pos].is_empty() == false) {
+        if(this->table[next_pos].is_removed() == false) {
+            if(this->table[next_pos].get_key() == key) {
+                return -1;
+            }
+        } else {
+            break;
+        }
+        i++;
+        next_pos = this->get_next_pos(pos, i);
+    }
+    this->table[next_pos].set_key_value(key, value);
+    this->size++;
+    if(this->get_load_factor() > 0.5) {
+        this->enlarge_table();
+    }
+    return i + 1;
 }
 
 template <typename K, typename V>
